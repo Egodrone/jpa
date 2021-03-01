4cb0bee42f955c9c73d7386885846b51c2860944
Eddie <eddie.ch.89d@gmail.com>
2021-03-01 16:46:30 +0100

    Person class

--- jpa/src/main/java/se/lexicon/jpa/entity/Person.java
+++ jpa/src/main/java/se/lexicon/jpa/entity/Person.java
@@ -1,5 +1,133 @@
 package se.lexicon.jpa.entity;
 
 
+import javax.persistence.Entity;
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+
+@Entity
 public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // means auto_increment
+    private int personId;
+    @Column(nullable = false, length = 50)
+    private String firstName;
+    @Column(nullable = false, length = 50)
+    private String lastName;
+    @Column(nullable = false, length = 50, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private LocalDate birthDate;
+    @Column(nullable = false, columnDefinition = "tinyint(1) default 1")
+    private boolean status;
+    private LocalDateTime registerDate;
+
+
+    public Person() {
+    }
+
+    public Person(int personId, String firstName, String lastName, String email, LocalDate birthDate, boolean status, LocalDateTime registerDate) {
+        this.personId = personId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.birthDate = birthDate;
+        this.status = status;
+        this.registerDate = registerDate;
+    }
+
+    public Person(String firstName, String lastName, String email, LocalDate birthDate, boolean status, LocalDateTime registerDate) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.birthDate = birthDate;
+        this.status = status;
+        this.registerDate = registerDate;
+    }
+
+    public int getPersonId() {
+        return personId;
+    }
+
+    public void setPersonId(int personId) {
+        this.personId = personId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getRegisterDate() {
+        return registerDate;
+    }
+
+    public void setRegisterDate(LocalDateTime registerDate) {
+        this.registerDate = registerDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return personId == person.personId && status == person.status && Objects.equals(firstName, person.firstName) && Objects.equals(lastName, person.lastName) && Objects.equals(email, person.email) && Objects.equals(birthDate, person.birthDate) && Objects.equals(registerDate, person.registerDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(personId, firstName, lastName, email, birthDate, status, registerDate);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "personId=" + personId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", birthDate=" + birthDate +
+                ", status=" + status +
+                ", registerDate=" + registerDate +
+                '}';
+    }
+
 }
